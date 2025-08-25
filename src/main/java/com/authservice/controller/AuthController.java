caf1daabddd75f46433936811032231f5208124d
Shahid Oulan Mansha <shahidoulanmansha@gmail.com>
2025-08-25 12:57:38 +0000

    login method in TokenController

--- src/main/java/com/authservice/controller/AuthController.java
+++ src/main/java/com/authservice/controller/AuthController.java
@@ -1,4 +1,48 @@
 package com.authservice.controller;
 
+
+import com.authservice.entities.RefreshToken;
+import com.authservice.model.UserInfoDto;
+import com.authservice.response.JwtResponseDTO;
+import com.authservice.service.JwtService;
+import com.authservice.service.RefreshTokenService;
+import com.authservice.service.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
 public class AuthController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @Autowired
+    private UserDetailsServiceImpl  userDetailsServiceImpl;
+
+    @PostMapping("auth/v1/signup")
+    public ResponseEntity SignUp(@RequestBody UserInfoDto userInfoDto){
+        try {
+            Boolean isSignUped = userDetailsServiceImpl.signUpUser(userInfoDto);
+            if (Boolean.FALSE.equals(isSignUped)) {
+                return new ResponseEntity<>("Already Exist", HttpStatus.BAD_REQUEST);
+            }
+
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(userInfoDto.getUserName());
+
+            String jwtToken = jwtService.GenerateToken(userInfoDto.getUserName());
+            return new ResponseEntity(JwtResponseDTO.builder().accessToken(jwtToken).token(refreshToken.getToken()).build(), HttpStatus.OK);
+        }catch (Exception ex) {
+            return new ResponseEntity<>("Exception in User Service", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- src/main/java/com/authservice/controller/TokenController.java
+++ src/main/java/com/authservice/controller/TokenController.java
@@ -1,4 +1,47 @@
 package com.authservice.controller;
 
+
+import com.authservice.entities.RefreshToken;
+import com.authservice.request.AuthRequestDTO;
+import com.authservice.response.JwtResponseDTO;
+import com.authservice.service.JwtService;
+import com.authservice.service.RefreshTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class TokenController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping("auth/v1/login")
+    public ResponseEntity AuthenticateAndGetToken(@RequestBody AuthRequestDTO authRequestDTO){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDTO.getUsername(), authRequestDTO.getPassword()));
+        if(authentication.isAuthenticated()){
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authRequestDTO.getUsername());
+            return new ResponseEntity<>(JwtResponseDTO.builder()
+                    .accessToken(jwtService.GenerateToken(authRequestDTO.getUsername()))
+                    .token(refreshToken.getToken())
+                    .build(), HttpStatus.OK
+            );
+        }else{
+            return new ResponseEntity<>("Exception in User Service", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
