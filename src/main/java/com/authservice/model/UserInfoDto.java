6d1322b0a0ff79b7cd76e5bb96c5a89da83944cd
ShahidOulanMansha <bcsf23m543@pucit.edu.pk>
2025-08-25 09:17:03 +0000

    UserInfoDto has been created

--- src/main/java/com/authservice/model/UserInfoDto.java
+++ src/main/java/com/authservice/model/UserInfoDto.java
@@ -1,4 +1,18 @@
 package com.authservice.model;
 
-public class UserInfoDto {
+
+import com.authservice.entities.UserInfo;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class UserInfoDto extends UserInfo {
+
+    public String firstName;
+
+    public String lastName;
+
+    public String email;
+    
+    public Long phoneNumber;
 }
--- src/main/java/com/authservice/service/UserDetailsServiceImpl.java
+++ src/main/java/com/authservice/service/UserDetailsServiceImpl.java
@@ -1,4 +1,35 @@
 package com.authservice.service;
 
-public class UserDetailsServiceImpl {
+import com.authservice.entities.UserInfo;
+import com.authservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserInfo userInfo = userRepository.findByUserName(username);
+        if (userInfo == null) {
+            throw new UsernameNotFoundException("could not found user...!!!");
+        }
+        return new CustomeUserDetails(userInfo);
+    }
 }
